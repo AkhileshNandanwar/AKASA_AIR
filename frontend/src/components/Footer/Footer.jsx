98ea84adc4c52150ca5f9251d2be066a0e59212e
AkhileshNandanwar <akhileshnandanwar751@gmail.com>
2024-09-15 15:04:30 +0530

    updated

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,41 +1,39 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import './Footer.css';
+import { assets } from '../../assets/assets';
 
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
+          <img src={assets.logo} alt="Akasa Air Logo" />
+          <p>SNV Aviation Private Limited (d/b/a Akasa Air), is an Indian low-cost airline headquartered in Mumbai, Maharashtra, India.It was founded by Vinay Dube and Aditya Ghosh with investor Rakesh Jhunjhunwala holding a 46% stake in the airline.
+
+The airline began commercial operation with its first flight service from Mumbai to Ahmedabad on 7 August 2022, after receiving its first Boeing 737 MAX 8 aircraft.The CEO of the airline, Vinay Dube, stated that the airline will have a fleet size of approximately 72 aircraft within 5 years. He stated that the airline will initially have services from metro cities to tier-2 and tier-3 cities, as well as operating flights to major cities across India.The airline currently has 25 aircraft flying to 27 destinations, with an order for additional 202 aircraft.</p>
+          
         </div>
         <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
         </div>
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+91-1122334455</li>
-                <li>akasaairfood@example.com</li>
-            </ul>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91-1122334455</li>
+            <li>akasaairfood@example.com</li>
+          </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © AkasaAIr.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 © AkasaAir.com - All Rights Reserved.</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
